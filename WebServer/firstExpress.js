f61a31d9478e7c3dd9567e44e47b4ee8eefa9897
halilozat <halilozat@hotmail.com>
2021-04-27 00:35:53 +0300

    http put and delete, joi module, postman

--- WebServer/firstExpress.js
+++ WebServer/firstExpress.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const joi = require('@hapi/joi')
 
 app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 
 const users = [
     {id:1,name:'halil',age:23},
@@ -38,19 +40,63 @@ app.get('/users/:id',(req,res) => {
 
 app.post('/users',(req,res) => {
 
-    const newUser = {
+    const {error} = confirmUserİnfo(req.body)
 
-        id: users.length +1,
-        name: req.body.name,
-        age: req.body.age
+    if(error){
+        res.status(400).send(error.details[0].message)
+    }else{
+        const newUser = {
+            id: users.length +1,
+            name: req.body.name,
+            age: req.body.age
+        }
+    
+        users.push(newUser)
+        res.send(newUser)
+    }
+})
 
+app.put('/users/:id',(req,res) => {
+
+    const fundUser = users.find(user => user.id === parseInt(req.params.id))
+   
+    if(!fundUser){
+        return res.status(404).send(`${req.params.id}'s user is not defined`)
     }
 
-    users.push(newUser)
-    res.send(newUser)
+    const {error} = confirmUserİnfo(req.body);
+    
+    if(error){
+        res.status(400).send(error.details[0].message)
+    }else{
+        fundUser.name = req.body.name,
+        fundUser.age = req.body.age
+    
+        res.send(fundUser)
+    }
+})
 
-} )
+app.delete('/users/:id',(req,res) => {
+    const fundUser = users.find(user => user.id === parseInt(req.params.id))
+    if(fundUser) {
+        const index = users.indexOf(fundUser)
+        users.splice(index, 1)
+        res.send(fundUser)
+    }else {
+        res.status(404).send('user is not found')
+    }
+})
 
+
+
+
+function confirmUserİnfo(user){
+    const schema = joi.object({
+        name: joi.string().min(3).max(30).required(),
+        age:joi.number().integer().min(1).max(99).required()
+    })
+    return schema.validate(user)
+}
 
 
 
