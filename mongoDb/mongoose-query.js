74756bab954a2bb3b19ed1e37028c67b3f7bfc7c
halilozat <halilozat@hotmail.com>
2021-04-29 02:36:09 +0300

    mongoose comparison and pagination

--- mongoDb/mongoose-query.js
+++ mongoDb/mongoose-query.js
@@ -5,7 +5,7 @@ mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnified
     console.log("Database connection is succesful");
 }).catch(err => console.log('BDatabase connection error:' + err));
 
-const UserSchema = new Schema({}, {collection:'user'})
+const UserSchema = new Schema({_id:String}, {collection:'user'})
 const User = mongoose.model('user', UserSchema)
 
 
@@ -13,3 +13,45 @@ const User = mongoose.model('user', UserSchema)
 User.find({age:23}, {name:1,address:1, _id:0}, {skip:3, limit:3}).then(allUsers => console.log(allUsers)).catch(err=>console.log(err))
 //or
 User.find({age:23}).limit(3).sort({name:1}).select({name:1,address:1}).then(allUsers => console.log(allUsers))
+
+
+User.findById('60898ec75ccc9e9eee0d8612').then(user=>console.log(user))
+User.findOne({age:23}).then(user=>console.log(user))
+
+//comparison operators
+
+//eq: equal
+//ne: not equal
+//gt: greater than
+//gte: greater  equal
+//lt: less than
+//lte: less equal
+//in
+//nin: not in
+
+
+//greater than 25, less than 35
+User.find({age: {$gte:25,$lte:35}}, {name:1,age:1}).then(users=>console.log(users))
+//age is 23 and 32
+User.find({age: {$in: [23,32]}}, {name:1,age:1}).then(users=>console.log(users))
+
+
+//and & or
+
+User.find()
+    .and([ {age:29},{eyeColo:'blue'} ])
+    .then(users=>console.log(users))
+
+    User.find()
+    .or([ {age:29},{eyeColo:'blue'} ])
+    .then(users=>console.log(users))
+
+//Pagination
+
+const pageNumber = 4
+const postPerPage = 10
+
+User.find()
+    .skip((pageNumber-1)*pageNumber)
+    .limit(postPerPage)
+    .then(result=>console.log(result))
